59a40df0e1acfd62ad1d7890620658ace52ea48e
Shamim <yeasinshamim7@gmail.com>
2022-04-22 10:24:53 +0600

    project init

---
+++ __steps.js
@@ -0,0 +1,30 @@
+/**
+ * ----------------------------
+ * for server side requirements
+ * ----------------------------
+ * 1. create folder and then write command npm init.
+ * 2. create file in the folder> index.js
+ * 3. then npm i express cors mongodb dotenv
+ * 
+ * ----------------------------
+ *  In index.js file
+ * ----------------------------
+ * 1. require express from express
+ * 2. require cors from cors.
+ * 3. require dotenv.
+ * 4. create app const
+ * 5. create a root get call 
+ * 6. create a port.
+ * 7. app.listening
+ * 
+ * ==============================
+ * Middleware
+ * ==============================
+ * 1. app.use(cors())
+ * 2. app.use(express/json())
+ * 
+ * =========================
+ * 1. add script in package.json file ("start": "node index.js")
+ * ("start-dev": "nodemon index.js");
+ * 
+ */
