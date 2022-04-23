5901d70392bf93aa1e818b17b56eb953a3bbffbf
Shamim <yeasinshamim7@gmail.com>
2022-04-23 11:02:24 +0600

    delete api added

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
+
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        // delete
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
         // await client.close();
